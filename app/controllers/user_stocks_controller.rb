a3e78b8efe04ba2f54acb7e83fe319dd55fad5b8
Naveen Nand <naveen@mywalletcard.com>
2021-02-04 13:29:51 -0800

    Add destroy stock

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -10,4 +10,13 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id)
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully deleted from your portfolio"
+    redirect_to my_portfolio_path
+  end
+  
 end
