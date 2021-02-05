fb01100655e7887fa0e130bface67d9afa831534
Naveen Nand <naveen@mywalletcard.com>
2021-02-04 16:58:36 -0800

    Fix destroy arguments error

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -14,9 +14,9 @@ class UserStocksController < ApplicationController
   def destroy
     stock = Stock.find(params[:id])
     user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id)
-    user_stock.destroy
+    user_stock.first.destroy
     flash[:notice] = "#{stock.ticker} was successfully deleted from your portfolio"
     redirect_to my_portfolio_path
   end
-  
+
 end
